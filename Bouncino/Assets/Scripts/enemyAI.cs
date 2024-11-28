6abb66b35760b446bd86df08aad4746e6979a8ae
Luke-Coching <luke.co1605@gmail.com>
2024-11-28 16:46:17 -0500

    Enemy Update
    >Enemy now moves from one point to the other
    >Enemy can kill the player (somewhat buggy)

--- Bouncino/Assets/Scripts/enemyAI.cs
+++ Bouncino/Assets/Scripts/enemyAI.cs
@@ -1,3 +1,4 @@
+using System.Drawing;
 using NUnit.Framework.Internal;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -6,15 +7,32 @@ public class enemyAI : MonoBehaviour
 {
     public GameObject player;
     public GameObject enemy;
+    public GameObject spawnPoint;
+    public AudioSource deathSound;
+    public GameObject PointA;
+    public GameObject PointB;
     private BoxCollider2D enemyCollider;
+    private Rigidbody2D rb;
+    private Transform currentPos;
     private Animator spikeAttack;
     int count = 0;
     int secondCount = 0;
+    public float speed;
+    bool isAttacking;
+
+    void Awake(){
+        enemyCollider = GetComponent<BoxCollider2D>();
+    }
+
     void Start(){
         spikeAttack = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        currentPos = PointB.transform;
+        isAttacking = false;
     }
 
     void Update(){
+        //Attack Sequence
         count++;
 
         if(count == 500){
@@ -22,21 +40,43 @@ public class enemyAI : MonoBehaviour
             count = 0;
         }
 
-        spikeAttack.SetInteger("Attack", secondCount);
+        if(secondCount > 3){
+            isAttacking = true;
+            spikeAttack.SetBool("Attack", isAttacking);
+        } else {
+            isAttacking = false;
+            spikeAttack.SetBool("Attack", isAttacking);
+        }
 
         if(secondCount == 7){
             secondCount = 0;
         }
-    }
+        //////////////////////
 
-    void Awake(){
-        enemyCollider = GetComponent<BoxCollider2D>();
+        //Movement Sequence
+        Vector2 point = currentPos.transform.position - transform.position;
+        if(currentPos == PointB.transform){
+            rb.linearVelocityX = speed;
+        } else {
+            rb.linearVelocityX = -speed;
+        }
+
+        if(Vector2.Distance(transform.position, currentPos.position) < 0.5f && currentPos == PointB.transform){
+            currentPos = PointA.transform;
+        }
+        
+        if(Vector2.Distance(transform.position, currentPos.position) < 0.5f && currentPos == PointA.transform){
+            currentPos = PointB.transform;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D other){
-        if(other.gameObject.CompareTag("Player")){
+        if(other.gameObject.CompareTag("Player") && !isAttacking){
             Destroy(enemy);
             enemyCollider.enabled = false;
+        } else if(other.gameObject.CompareTag("Player") && isAttacking) {
+            player.transform.position = spawnPoint.transform.position;
+            deathSound.Play();
         }
     }
    
