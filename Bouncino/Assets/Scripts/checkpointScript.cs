462b1c805713cf7bf04d5896efedef7a8e0cc53b
Luke-Coching <luke.co1605@gmail.com>
2024-12-05 16:58:28 -0500

    Changes
    >Added checkpoint sprite

--- Bouncino/Assets/Scripts/checkpointScript.cs
+++ Bouncino/Assets/Scripts/checkpointScript.cs
@@ -4,16 +4,22 @@ public class checkpointScript : MonoBehaviour
 {
     public Transform startPoint;
     public Transform checkPoint;
-    private BoxCollider2D checkPointCollider;   
+    private BoxCollider2D checkPointCollider;
+    private Animator checkpointCaptured;
 
     void Awake(){
         checkPointCollider = GetComponent<BoxCollider2D>();
+    }
+
+    void Start(){
+        checkpointCaptured = GetComponent<Animator>();
     }
 
     private void OnTriggerEnter2D(Collider2D other){
         if(other.gameObject.CompareTag("Player")){
             startPoint.transform.position = checkPoint.transform.position;
             checkPointCollider.enabled = false;
+            checkpointCaptured.SetBool("Captured", true);
         }
     }
 }
